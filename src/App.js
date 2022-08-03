ee6ceb9f887a466869613a8ddafe260fcd159e76
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-03 10:53:08 -0600

    Add api call and component did mount to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { fetchSchoolsInfo } from './api-call';
 import './App.css';
 
 class App extends Component {
+  state = {
+    schools: []
+  }
+
+  componentDidMount() {
+    fetchSchoolsInfo()
+      .then((data) => {
+        this.setState({schools: data.results})
+      })
+  }
 
   render() {
     return (
