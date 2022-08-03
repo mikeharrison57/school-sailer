3783acadcd428e87482648341eb4ed3c3e267e9e
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-03 16:25:12 -0600

    Add state data file with state array

--- src/Application/StateForm/StateSelectionForm.js
+++ src/Application/StateForm/StateSelectionForm.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const StateSelectionForm = () => {
-  
+
+  const [name, setName] = useState('')
+  const [state, setState] = useState('')
+
+  // const stateOptions =
+
+  return (
+    <form className='state-form'>
+      <input
+        type='text'
+        placeHolder='Name'
+        onChange={event => setName(event.target.value)}
+        required
+      />
+      <select onChange={event => setName(event.target.value)} required>
+
+       
+      
+      </select>
+    </form>
+  )
+
 }
 
 export default StateSelectionForm;
