189111db29e0e190d76c412adaa8d2d163928ab9
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-04 22:35:15 -0600

    Basic info displayed after clicking college card

--- src/Application/SchoolDetailPage/SchoolDetailPage.js
+++ src/Application/SchoolDetailPage/SchoolDetailPage.js
@@ -1,51 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import './SchoolDetailPage.css';
 
 const SchoolDetailPage = ({ schoolName, lists }) => {
 
-  const [school, setSchool] = useState({});
+  const [list, setList] = useState({});
 
   const getSchool = () => {
-    const selectedSchool = lists.find(list => list.latest.school.name === schoolName);
-    const updatedSchool = {...selectedSchool};
-    setSchool( school => ({
-      ...school,
-      ...updatedSchool
+    const selectedList = lists.find(list => list.latest.school.name === schoolName);
+    const updatedList = {...selectedList};
+    setList( list => ({
+      ...list,
+      ...updatedList
     }));
   }
-
+  
   useEffect(() => {
     getSchool();
+    // clearList()
   }, [lists, schoolName])
 
-  
+  // const clearList = () => {
+  //   setList({})
+  // }
+
   return (
-    <section className='school-detail-page'>
-      {console.log(school)}
-      <header className='school-header'>
-        <h2>School Name:</h2>
-        {/* <img></img> */}
-        <p>City:</p>
-      </header>
-      <section className='primary-info'>
-        <article className='degree-categories'>
+    <>
+    {/* {list && */}
+      <section className='school-detail-content'>
+        {/* {console.log(list.school.city)} */}
+        <header className='school-header'>
+          <h2>{schoolName}</h2>
           {/* <img></img> */}
-          <ul>
-            <li>Degree Categories:</li>
-          </ul>
-        </article>
-        <article className='cost-info'>
-          {/* <img></img> */}
-          <ul>
-            <li>Cost Info:</li>
-          </ul>
-        </article>
-        <article className='stats'>
-          <ul>
-            <li>Stats:</li>
-          </ul>
-        </article>
+          {/* <p>{`${list.school.city}, ${list.school.state}`}</p> */}
+          {/* <a href={list.school.school_url}>{list.school.school_url}</a> */}
+        </header>
+        <section className='primary-info'>
+          <article className='degree-categories'>
+            {/* <img></img> */}
+            <ul>
+              <li>Degree Categories:</li>
+            </ul>
+          </article>
+          <article className='cost-info'>
+            {/* <img></img> */}
+            <ul>
+              <li>Cost Info:</li>
+            </ul>
+          </article>
+          <article className='stats'>
+            <ul>
+              <li>Stats:</li>
+            </ul>
+          </article>
+        </section>
       </section>
-    </section>
+      
+    </>
   )
 }
 
