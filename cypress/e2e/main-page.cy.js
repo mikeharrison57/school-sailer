d617ab139e148810d6ad860109701019e21c8b78
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-06 21:49:30 -0600

    Fist Navbar test

--- cypress/e2e/main-page.cy.js
+++ cypress/e2e/main-page.cy.js
@@ -1,5 +1,17 @@
+const secureKey = process.env.REACT_APP_SCHOOLS_API_KEY;
+const primaryUrl = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?'
+
 describe('main page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
+    // cy.intercept('GET', `${primaryUrl}school.state=CO&${secureKey}`, {fixture: 'colorado-schools'})
   })
+
+  it('Should have the heading School Sailor in the navbar with a sailboat icon next to it.', () => {
+    cy.get('h1').should('have.text', 'School Sailor')
+    cy.get('.sailboat').should('be.visible')
+  })
+
+  
+
 })
