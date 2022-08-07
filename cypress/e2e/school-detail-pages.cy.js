11cc2046b7bd2254155723ab11d585b68fc7960c
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-07 16:48:39 -0600

    Wrote test for school details page content

--- cypress/e2e/school-detail-pages.cy.js
+++ cypress/e2e/school-detail-pages.cy.js
@@ -14,4 +14,22 @@ describe('state pages', () => {
     cy.get('.home-container').should('exist')
     cy.get('.favorites-container').should('exist')
   })
+
+
+  it('Should fetch and display additional school information from user selected school.', () => {
+    cy.intercept('GET', `${primaryUrl}school.state=GA&${secureKey}`)
+    cy.visit('http://localhost:3000/GA')
+    cy.get('.school-container > :nth-child(18)').find('.more-info').click()
+    cy.intercept('GET', `${primaryUrl}school.name=University%20of%20Colorado%20Colorado%20Springs&${secureKey}`)
+    cy.url('should.eq', 'http://localhost:3000/GA/Berry%20College')
+    cy.get('.school-header').find('h2').should('have.text', 'Berry College')
+    cy.get('.cost-info').should('contain', 'Campus Housing: $13370')
+    cy.get('.stats').should('contain', 'Admission Rate: 77.22 %' )
+  })
+
+  it('Should be able to fetch and display a different school and information about a different school.' , () => {
+    
+  })
+
+  // it('Should be able to navigate to and invidual school ')
 })
