2db79ddfa00a0d16eb76c08c8ce06b9aabb3b439
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-07 00:01:27 -0600

    Tested content on different state page

--- cypress/e2e/state-pages.cy.js
+++ cypress/e2e/state-pages.cy.js
@@ -7,7 +7,7 @@ describe('state pages', () => {
   })
 
   it.skip('Should have the heading School Sailor, a sailboat icon, home button, and favorites button in the Navbar.', () => {
-    cy.intercept('GET', `${primaryUrl}school.state=CO&${secureKey}`, {fixture: 'colorado-schools'})
+    cy.intercept('GET', `${primaryUrl}school.state=CO&${secureKey}`)
     cy.visit('http://localhost:3000/CO')
     cy.get('h1').should('have.text', 'School Sailor')
     cy.get('.sailboat').should('be.visible')
@@ -17,16 +17,29 @@ describe('state pages', () => {
 
   it.skip('Should fetch and display schools from user selected state', () => {
     cy.get('select').select('Hawaii')
-    cy.intercept('GET', `${primaryUrl}school.state=HI&${secureKey}`, {fixture: 'colorado-schools'})
     cy.get('.state-button').click()
+    cy.intercept('GET', `${primaryUrl}school.state=HI&${secureKey}`)
+    cy.url('should.eq', 'http://localhost:3000/HI')
     cy.get('.school-card').should('have.length', 20).should('not.be.empty')
   })
 
-  it('Should have basic information about each school displayed on each school card', () => {
-    cy.intercept('GET', `${primaryUrl}school.state=WA&${secureKey}`, {fixture: 'colorado-schools'})
+  it.skip('Should have basic information about each school displayed on each school card', () => {
+    cy.intercept('GET', `${primaryUrl}school.state=WA&${secureKey}`)
     cy.visit('http://localhost:3000/WA')
     cy.get('.school-card').find('h3').first().should('have.text', 'Name: Charter College')
     cy.get('.school-container > :nth-child(1) > :nth-child(3)').should('have.text', 'City: Vancouver, WA')
     cy.get('.school-container > :nth-child(19)').should('exist').should('contain.text', 'Average Cost of Attendance Per Year: $14465')
   })
+
+  it.skip('Should be able to fetch and dispaly schools from a different state' , () => {
+    cy.intercept('GET', `${primaryUrl}school.state=AK&${secureKey}`)
+    cy.visit('http://localhost:3000/AK')
+    cy.get('.school-card').should('have.length', 9).should('not.be.empty')
+    cy.get('.school-card').last().should('exist')
+    cy.get(':nth-child(8) > h3').should('have.text', 'Name: Ilisagvik College')
+    cy.get(':nth-child(8) > :nth-child(4)').should('have.text', 'Average Cost of Attendance Per Year: Currently Unavailable')
+    cy.get('.school-container > :nth-child(2) > :nth-child(3)').should('have.text', 'City: Palmer, AK')
+    
+  })
+
 })
