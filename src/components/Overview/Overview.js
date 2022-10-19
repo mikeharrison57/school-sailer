5bb3c2c9420cdea1ee3a0879ad10a08418487ae9
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-10-19 17:18:36 -0600

    Add Ovierview component

---
+++ src/components/Overview/Overview.js
@@ -0,0 +1,18 @@
+const Overview = () => {
+  return (
+		<p>
+			You're the captain of your future! School Sailor is a casual college and
+			trade school browsing application. It’s meant for people who aren’t entirely 
+      sure about what college or tradeschool they might be interested in, but would 
+      like to casually cruise or sail through possible options. 
+      You can use the search box above to search for schools based on state. You can
+      favorite schools by clikcing on the banner icon in the upper right hand of each
+      school. Click the See More Info button to see more information about the school. 
+      Finally, so see schools that you've favorited click the Favorites button in the upper
+      right hand corner of the navigation bar. Best of luck out there sailor, you've got a 
+      bright future ahead!
+		</p>
+	);
+}
+
+export default Overview;
